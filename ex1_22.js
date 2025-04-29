9e33a2e1e7f8d1fd712dec24bcc1bc58f336edfa
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-04-29 22:16:21 +0900

    ex1_22 undefined와 null의 비교

---
+++ ex1_22.js
@@ -0,0 +1,10 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var n = null;
+console.log(typeof n); // object
+
+console.log(n == undefined); // true
+console.log(n == null); // true
+
+console.log(n === undefined); // false
+console.log(n === null); // true
