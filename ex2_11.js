4e5087638ff0590d84c690b65f16b1e3ac2dc272
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-01 08:39:30 +0900

    ex2_11 함수 선언문의 위험성

---
+++ ex2_11.js
@@ -0,0 +1,18 @@
+// Core JavaScript Practice by Seungmin Kim
+
+console.log(sum(3, 4));
+
+function sum(x, y) {
+  return x + y;
+}
+
+//var a = sum(1, 2);
+var a = sum(5, 6);
+console.log(a);
+
+function sum(x, y) {
+  return x + ' + ' + y + ' = ' + (x + y);
+}
+
+var c = sum(1, 2);
+console.log(c);
