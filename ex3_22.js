843eceb64cf21b3bbb7eed6caa05ba04ce1e4185
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-01 20:19:10 +0900

    ex3_22 call/apply 메서드의 활용(3-1)

---
+++ ex3_22.js
@@ -0,0 +1,13 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var numbers = [10, 20, 3, 16, 45];
+var max = (min = numbers[0]);
+numbers.forEach(function(number) {
+  if (number > max) {
+    max = number;
+  }
+  if (number < min) {
+    min = number;
+  }
+});
+console.log(max, min); // 45 3
