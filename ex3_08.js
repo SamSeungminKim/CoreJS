d420e6fbb19cb73339622f305d058eb65646fdc5
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-01 16:19:10 +0900

    ex3_08 메서드 내부에서의 this

---
+++ ex3_08.js
@@ -0,0 +1,19 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var obj = {
+  methodA: function() {
+    console.log(this);
+  },
+  inner: {
+    methodB: function() {
+      console.log(this);
+    },
+  },
+};
+obj.methodA(); // { methodA: f, inner: {...} }    ( === obj)
+obj['methodA'](); // { methodA: f, inner: {...} } ( === obj)
+
+obj.inner.methodB(); // { methodB: f }            ( === obj.inner)
+obj.inner['methodB'](); // { methodB: f }         ( === obj.inner)
+obj['inner'].methodB(); // { methodB: f }         ( === obj.inner)
+obj['inner']['methodB'](); // { methodB: f }      ( === obj.inner)
