1ee4af6dc69ecea27edaba3dc0eddf766a217dc9
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-01 19:06:01 +0900

    ex3_15 call 메서드(2)

---
+++ ex3_15.js
@@ -0,0 +1,11 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var obj = {
+  a: 1,
+  method: function(x, y) {
+    console.log(this.a, x, y);
+  },
+};
+
+obj.method(2, 3); // 1 2 3
+obj.method.call({ a: 4 }, 5, 6); // 4 5 6
