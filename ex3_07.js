9d98f43c226f0d391ea49c05c4c86b008b1b038c
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-01 16:06:41 +0900

    ex3_07 메서드로서의 호출

---
+++ ex3_07.js
@@ -0,0 +1,9 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var obj = {
+  method: function(x) {
+    console.log(this, x);
+  },
+};
+obj.method(1); // { method: f } 1
+obj['method'](2); // { method: f } 2
